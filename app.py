c48a9941bb2d57bfff463aa2428249ccd3da2169
Jack Peterson <jack@tinybike.net>
2014-10-28 18:25:09 -0700

    Example tx jsons

--- app.py
+++ app.py
@@ -178,7 +178,26 @@ def create_jury(name):
 
 @socketio.on('add-event', namespace='/socket.io/')
 def add_event(args):
+    """Example:
+    
+    $ ./truth_cli.py ask_decision j_1 world_end_this_week is the world going to end sometime this week
 
+    Output:
+    {
+       "count": 793, 
+       "decision_id": "world_end_this_week", 
+       "pubkeys": [
+          "04fe2654f07ffe0c66529707762aabebbec19870aaa36dbc503526a556e55c4926f093a23596337bce001a5a219d800917359f4bc5d5ed58a727243580ce1d2e20"
+       ], 
+       "signatures": [
+          "G8oZq7bqXskySI/HCV4n9xmqe0q+n0j3TFQfCTZvwgLRkOGHKwiBCQF7kATHna5a6aEqZ6fWbBqUoAoHHs9LLV0="
+       ], 
+       "txt": "world_end_this_week is the world going to end sometime this week", 
+       "type": "propose_decision", 
+       "vote_id": "j_1"
+    }
+
+    """
     data = node.send({ 'command':['ask_decision', args['juryId'], args['eventId'], '"'+args['eventText']+'"'] })
 
     app.logger.info(data)
@@ -187,8 +206,33 @@ def add_event(args):
 @socketio.on('buy-shares', namespace='/socket.io/')
 def buy_shares(args):
     """Example:
+
+    ./truth_cli.py info world_ending_PM
+    {
+       "B": 1000, 
+       "author": "11gt9t8wqqmBPt8rSmAnhcyvwA2QrpM", 
+       "decisions": [
+          "world_end_this_week"
+       ], 
+       "fees": 0, 
+       "shares_purchased": [
+          0, 
+          0
+       ], 
+       "states": [
+          "yes", 
+          "no"
+       ], 
+       "states_combinatory": [
+          [
+             1
+          ]
+       ]
+    }
+
+    $ ./truth_cli.py buy_shares
     What is the unique name for this prediction market?
-    >weatherPM1
+    >world_ending_PM
     how many states does this pm have?
     >2
     how many shares do you want to buy of state 0? To sell states, use negative numbers.
@@ -196,8 +240,36 @@ def buy_shares(args):
     how many shares do you want to buy of state 1? To sell states, use negative numbers.
     >0
     What is your brainwallet
-    >
-    
+    >my_password
+    now for a little proof of work. This may take several minutes. The purpose of this pow is to make it more difficult for a front runner to steal your trade.
+    tx for copy/pasting into pushtx: eyJjb3VudCI6IDc5OCwgIm5vbmNlIjogNDQ4MDk3OTgwNjI3MTk5NzUzNDg3MjkxNTE5NzUyNDU0
+    MTUwNzA2OSwgImJ1eSI6IFsxMCwgMF0sICJ0eXBlIjogImJ1eV9zaGFyZXMiLCAiUE1faWQiOiAi
+    d29ybGRfZW5kaW5nX1BNIiwgInNpZ25hdHVyZXMiOiBbIkhBUngrUzlLcjZ3cEtIbDJHdm9YYTFR
+    a3I2bS8vYWY1dUVaZ05OQ1BncHhZR1EzZlVpc0pab2RSSUFOaURvMFRWT0RTRkdYNkx2WSs3bWhn
+    eWlod3dMZz0iXSwgInB1YmtleXMiOiBbIjA0ZmUyNjU0ZjA3ZmZlMGM2NjUyOTcwNzc2MmFhYmVi
+    YmVjMTk4NzBhYWEzNmRiYzUwMzUyNmE1NTZlNTVjNDkyNmYwOTNhMjM1OTYzMzdiY2UwMDFhNWEy
+    MTlkODAwOTE3MzU5ZjRiYzVkNWVkNThhNzI3MjQzNTgwY2UxZDJlMjAiXSwgInByaWNlX2xpbWl0
+    IjogNX0=
+
+    added tx: 
+    {
+       "PM_id": "world_ending_PM", 
+       "buy": [
+          10, 
+          0
+       ], 
+       "count": 798, 
+       "nonce": 4480979806271997534872915197524541507069, 
+       "price_limit": 5, 
+       "pubkeys": [
+          "04fe2654f07ffe0c66529707762aabebbec19870aaa36dbc503526a556e55c4926f093a23596337bce001a5a219d800917359f4bc5d5ed58a727243580ce1d2e20"
+       ], 
+       "signatures": [
+          "HARx+S9Kr6wpKHl2GvoXa1Qkr6m//af5uEZgNNCPgpxYGQ3fUisJZodRIANiDo0TVODSFGX6LvY+7mhgyihwwLg="
+       ], 
+       "type": "buy_shares"
+    }
+
     """
     privkey = tools.db_get("privkey")
     pubkey = tools.privtopub(privkey)
@@ -222,30 +294,37 @@ def buy_shares(args):
 @socketio.on('make-pm', namespace='/socket.io/')
 def make_pm(args):
     """Example:
+    $ ./truth_cli.py make_PM
     What is the address or pubkey of the owner of the PM?
     >11gt9t8wqqmBPt8rSmAnhcyvwA2QrpM
     What is the unique name for this new prediction market?
-    >weatherPM1
+    >world_ending_PM
     how big should B be? Initial investment is B*ln(n) where n is the number of states
-    >10000
+    >1000
     how many decisions is this prediction market to be based upon?
     >1
     What is the unique name of the 0 decision?
-    >what
+    >world_end_this_week
     how many states can this PM result in?
     >2
     what is the text title of the 0 state?
-    >rain
+    >yes
     how does the 0 state depend upon the outcome of the decisions? For example: if there are 2 decisions, and this market only comes true when the first is "yes" and the second is "no", then you would put: "1 0" here.
     >1
     what is the text title of the 1 state?
-    >sun
+    >no
+    tx for copy/pasting into pushtx: eyJCIjogMTAwMCwgInR5cGUiOiAicHJlZGljdGlvbl9tYXJrZXQiLCAic3RhdGVzIjogWyJ5ZXMi
+    LCAibm8iXSwgIlBNX2lkIjogIndvcmxkX2VuZGluZ19QTSIsICJmZWVzIjogMCwgIm93bmVyIjog
+    IjExZ3Q5dDh3cXFtQlB0OHJTbUFuaGN5dndBMlFycE0iLCAiZGVjaXNpb25zIjogWyJ3b3JsZF9l
+    bmRfdGhpc193ZWVrIl0sICJzdGF0ZXNfY29tYmluYXRvcnkiOiBbWzFdXX0=
+
+    added tx: 
     {
-       "B": 10000, 
-       "PM_id": "weatherPM1", 
-       "count": 714, 
+       "B": 1000, 
+       "PM_id": "world_ending_PM", 
+       "count": 794, 
        "decisions": [
-          "what"
+          "world_end_this_week"
        ], 
        "fees": 0, 
        "owner": "11gt9t8wqqmBPt8rSmAnhcyvwA2QrpM", 
@@ -253,11 +332,11 @@ def make_pm(args):
           "04fe2654f07ffe0c66529707762aabebbec19870aaa36dbc503526a556e55c4926f093a23596337bce001a5a219d800917359f4bc5d5ed58a727243580ce1d2e20"
        ], 
        "signatures": [
-          "G8fGpV5fF8QDOVgC/1QOtORMv4tMG/wQL7z6xEgNu+ArkHIRyHZvBECXDipUduXJLB0RiyKtZHhkzV78Yi/VJQg="
+          "HDpZ0bBHP4m3P0MgxCCUtTv7R+/H6UcLKwscFBJAmwodZRnopdG5xNqGaf2Lwq9idyaEEKbJBRJzO4bzqzvWfBo="
        ], 
        "states": [
-          "rain", 
-          "sun"
+          "yes", 
+          "no"
        ], 
        "states_combinatory": [
           [
