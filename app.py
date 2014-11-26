bd586a7b8da35c81364d0f7013075a49d79032a1
Jack Peterson <jack@tinybike.net>
2014-11-25 23:58:02 -0800

    Cleaned up gitpython paths, debug messages

--- app.py
+++ app.py
@@ -52,19 +52,16 @@ class Api(object):
         if not os.path.isdir(self.core_path):
             self.core_path = os.path.join(HERE, os.pardir, "Truthcoin-POW")
             if not os.path.isdir(self.core_path):
-                app.logger.debug("Augur core not found, downloading...")
-                core_git_url = "https://github.com/AugurProject/AugurCore.git"
                 self.core_path = os.path.join(HERE, os.pardir, "AugurCore")
+                core_git_url = "https://github.com/AugurProject/AugurCore.git"
+                app.logger.debug("Augur core not found.  Cloning " +\
+                                 core_git_url + " to " + str(self.core_path))
                 os.mkdir(self.core_path)
                 repo = git.Repo.init(self.core_path)
                 origin = repo.create_remote("origin", core_git_url)
                 origin.fetch()
                 origin.pull(origin.refs[0].remote_head)
-                app.logger.debug("Installed new core: " + str(self.core_path))
-            else:
-                app.logger.debug("Found core: " + str(self.core_path))
-        else:
-            app.logger.debug("Found core: " + str(self.core_path))
+                app.logger.debug("Installed core: " + str(self.core_path))
 
     @property
     def python_cmd(self):
