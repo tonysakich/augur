7ae6261878685077864e79c402f64556eb965d7a
Joseph Krug <joeykrug@gmail.com>
2014-11-28 12:17:57 -0800

    deleted augur shell -- realized this should be a download on the website for newbies, not in git repo

--- Augur_Shell_Install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-killall python
-killall Python
-rm -rf augur
-git clone https://github.com/AugurProject/augur.git
-cd augur
-pip install -r requirements.txt
-echo "Augur started, go to http://127.0.0.1:9000/ in your web browser"
-./augur_ctl start
