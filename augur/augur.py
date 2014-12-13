d2fb8ceda04941fb8974cc9703324e2758d2f2f6
Jack Peterson <jack@tinybike.net>
2014-12-13 00:08:28 -0800

    bump to version 0.2

--- augur/augur.py
+++ augur/augur.py
@@ -32,7 +32,7 @@ import base64
 from six.moves import xrange as range
 
 __title__      = "augur"
-__version__    = "0.1.1"
+__version__    = "0.2"
 __author__     = "Scott Leonard, Jack Peterson, Chris Calderon"
 __license__    = "MIT"
 __maintainer__ = "Scott Leonard"
